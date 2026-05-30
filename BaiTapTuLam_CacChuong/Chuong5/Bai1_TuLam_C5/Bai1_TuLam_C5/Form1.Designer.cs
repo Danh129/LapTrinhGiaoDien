8976b392f661085dbd594ab366d040c1ed358321
Danh129 <lethanhdanh2006129@gmail.com>
2026-05-30 21:15:26 +0700

    Add files via upload

---
+++ BaiTapTuLam_CacChuong/Chuong5/Bai1_TuLam_C5/Bai1_TuLam_C5/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Bai1_TuLam_C5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.picThanh = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picThanh)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // picBall
+            // 
+            this.picBall.BackColor = System.Drawing.Color.White;
+            this.picBall.Image = global::Bai1_TuLam_C5.Properties.Resources.Bong;
+            this.picBall.Location = new System.Drawing.Point(236, 114);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(100, 96);
+            this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBall.TabIndex = 0;
+            this.picBall.TabStop = false;
+            // 
+            // picThanh
+            // 
+            this.picThanh.BackColor = System.Drawing.Color.Silver;
+            this.picThanh.Location = new System.Drawing.Point(207, 359);
+            this.picThanh.Name = "picThanh";
+            this.picThanh.Size = new System.Drawing.Size(153, 16);
+            this.picThanh.TabIndex = 1;
+            this.picThanh.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picThanh);
+            this.Controls.Add(this.picBall);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picThanh)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.PictureBox picThanh;
+    }
+}
+
---
+++ BaiTapTuLam_CacChuong/Chuong5/Bai1_TuLam_C5/Bai1_TuLam_C5/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai1_TuLam_C5
+{
+    public partial class Form1 : Form
+    {
+        // Toc do bong
+        int dx = 5;
+        int dy = 5;
+
+        // Kiem tra start/stop
+        bool dangChay = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Di chuyen bong
+            picBall.Left += dx;
+            picBall.Top += dy;
+
+            // Cham tuong trai/phai
+            if (picBall.Left <= 0 ||
+                picBall.Right >= this.ClientSize.Width)
+            {
+                dx = -dx;
+            }
+
+            // Cham phia tren
+            if (picBall.Top <= 0)
+            {
+                dy = -dy;
+            }
+
+            // Va vao thanh ngang
+            if (picBall.Bounds.IntersectsWith(picThanh.Bounds))
+            {
+                dy = -dy;
+            }
+
+            // Rot xuong duoi
+            if (picBall.Bottom >= this.ClientSize.Height)
+            {
+                timer1.Stop();
+                MessageBox.Show("Game Over");
+                Application.Exit();
+                
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Qua trai
+            if (e.KeyCode == Keys.Left)
+            {
+                picThanh.Left -= 20;
+            }
+
+            // Qua phai
+            if (e.KeyCode == Keys.Right)
+            {
+                picThanh.Left += 20;
+            }
+
+            // Start / Stop bang phim S
+            if (e.KeyCode == Keys.S)
+            {
+                if (dangChay == false)
+                {
+                    timer1.Start();
+                    dangChay = true;
+                }
+                else
+                {
+                    timer1.Stop();
+                    dangChay = false;
+                }
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            picThanh.Left = e.X - picThanh.Width / 2;
+        }
+    }
+}
