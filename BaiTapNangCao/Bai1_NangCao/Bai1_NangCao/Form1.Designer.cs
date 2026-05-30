ad9145e13a8bc9c600b68983c8f74757dc46d9cb
Danh129 <lethanhdanh2006129@gmail.com>
2026-05-30 20:50:45 +0700

    Add files via upload

---
+++ BaiTapNangCao/Bai1_NangCao/Bai1_NangCao/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Bai1_NangCao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pic0 = new System.Windows.Forms.PictureBox();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.pic5 = new System.Windows.Forms.PictureBox();
+            this.pic6 = new System.Windows.Forms.PictureBox();
+            this.pic7 = new System.Windows.Forms.PictureBox();
+            this.pic8 = new System.Windows.Forms.PictureBox();
+            this.pic9 = new System.Windows.Forms.PictureBox();
+            this.pic10 = new System.Windows.Forms.PictureBox();
+            this.pic11 = new System.Windows.Forms.PictureBox();
+            this.pic12 = new System.Windows.Forms.PictureBox();
+            this.pic13 = new System.Windows.Forms.PictureBox();
+            this.pic14 = new System.Windows.Forms.PictureBox();
+            this.pic15 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic13)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic14)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic15)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 234F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 225F));
+            this.tableLayoutPanel1.Controls.Add(this.pic0, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pic1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pic2, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pic3, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pic4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pic5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pic6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pic7, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pic8, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pic9, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pic10, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.pic12, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pic13, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pic14, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pic15, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.pic11, 3, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 160F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 163F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 151F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(933, 629);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pic0
+            // 
+            this.pic0.Location = new System.Drawing.Point(3, 3);
+            this.pic0.Name = "pic0";
+            this.pic0.Size = new System.Drawing.Size(231, 149);
+            this.pic0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic0.TabIndex = 0;
+            this.pic0.TabStop = false;
+            // 
+            // pic1
+            // 
+            this.pic1.Location = new System.Drawing.Point(240, 3);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(231, 149);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 1;
+            this.pic1.TabStop = false;
+            // 
+            // pic2
+            // 
+            this.pic2.Location = new System.Drawing.Point(477, 3);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(228, 149);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 2;
+            this.pic2.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.Location = new System.Drawing.Point(711, 3);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(219, 149);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic3.TabIndex = 3;
+            this.pic3.TabStop = false;
+            // 
+            // pic4
+            // 
+            this.pic4.Location = new System.Drawing.Point(3, 158);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(231, 154);
+            this.pic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic4.TabIndex = 4;
+            this.pic4.TabStop = false;
+            // 
+            // pic5
+            // 
+            this.pic5.Location = new System.Drawing.Point(240, 158);
+            this.pic5.Name = "pic5";
+            this.pic5.Size = new System.Drawing.Size(231, 154);
+            this.pic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic5.TabIndex = 5;
+            this.pic5.TabStop = false;
+            // 
+            // pic6
+            // 
+            this.pic6.Location = new System.Drawing.Point(477, 158);
+            this.pic6.Name = "pic6";
+            this.pic6.Size = new System.Drawing.Size(228, 154);
+            this.pic6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic6.TabIndex = 6;
+            this.pic6.TabStop = false;
+            // 
+            // pic7
+            // 
+            this.pic7.Location = new System.Drawing.Point(711, 158);
+            this.pic7.Name = "pic7";
+            this.pic7.Size = new System.Drawing.Size(219, 154);
+            this.pic7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic7.TabIndex = 7;
+            this.pic7.TabStop = false;
+            // 
+            // pic8
+            // 
+            this.pic8.Location = new System.Drawing.Point(3, 318);
+            this.pic8.Name = "pic8";
+            this.pic8.Size = new System.Drawing.Size(231, 157);
+            this.pic8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic8.TabIndex = 8;
+            this.pic8.TabStop = false;
+            // 
+            // pic9
+            // 
+            this.pic9.Location = new System.Drawing.Point(240, 318);
+            this.pic9.Name = "pic9";
+            this.pic9.Size = new System.Drawing.Size(231, 157);
+            this.pic9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic9.TabIndex = 9;
+            this.pic9.TabStop = false;
+            // 
+            // pic10
+            // 
+            this.pic10.Location = new System.Drawing.Point(477, 318);
+            this.pic10.Name = "pic10";
+            this.pic10.Size = new System.Drawing.Size(228, 157);
+            this.pic10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic10.TabIndex = 10;
+            this.pic10.TabStop = false;
+            // 
+            // pic11
+            // 
+            this.pic11.Location = new System.Drawing.Point(711, 318);
+            this.pic11.Name = "pic11";
+            this.pic11.Size = new System.Drawing.Size(219, 157);
+            this.pic11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic11.TabIndex = 11;
+            this.pic11.TabStop = false;
+            // 
+            // pic12
+            // 
+            this.pic12.Location = new System.Drawing.Point(3, 481);
+            this.pic12.Name = "pic12";
+            this.pic12.Size = new System.Drawing.Size(231, 145);
+            this.pic12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic12.TabIndex = 12;
+            this.pic12.TabStop = false;
+            // 
+            // pic13
+            // 
+            this.pic13.Location = new System.Drawing.Point(240, 481);
+            this.pic13.Name = "pic13";
+            this.pic13.Size = new System.Drawing.Size(231, 145);
+            this.pic13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic13.TabIndex = 13;
+            this.pic13.TabStop = false;
+            // 
+            // pic14
+            // 
+            this.pic14.Location = new System.Drawing.Point(477, 481);
+            this.pic14.Name = "pic14";
+            this.pic14.Size = new System.Drawing.Size(228, 145);
+            this.pic14.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic14.TabIndex = 14;
+            this.pic14.TabStop = false;
+            // 
+            // pic15
+            // 
+            this.pic15.Location = new System.Drawing.Point(711, 481);
+            this.pic15.Name = "pic15";
+            this.pic15.Size = new System.Drawing.Size(219, 145);
+            this.pic15.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic15.TabIndex = 15;
+            this.pic15.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 750;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 629);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Truc Xinh";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pic0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic13)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic14)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic15)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pic0;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic5;
+        private System.Windows.Forms.PictureBox pic6;
+        private System.Windows.Forms.PictureBox pic7;
+        private System.Windows.Forms.PictureBox pic8;
+        private System.Windows.Forms.PictureBox pic9;
+        private System.Windows.Forms.PictureBox pic10;
+        private System.Windows.Forms.PictureBox pic11;
+        private System.Windows.Forms.PictureBox pic12;
+        private System.Windows.Forms.PictureBox pic13;
+        private System.Windows.Forms.PictureBox pic14;
+        private System.Windows.Forms.PictureBox pic15;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BaiTapNangCao/Bai1_NangCao/Bai1_NangCao/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai1_NangCao
+{
+    public partial class Form1 : Form
+    {
+        //Danh sách chứa các ảnh
+        List<Image> icons = new List<Image>();
+        //Lưu ô đầu
+        PictureBox firstClicked = null;
+        //Lưu ô thứ 2
+        PictureBox secondClicked = null;
+
+        // Timer dùng để úp hình lại khi chọn sai
+        Timer gameTimer = new Timer();
+
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            gameTimer.Interval = 750;
+            // Gán sự kiện Tick cho timer
+            gameTimer.Tick += gameTimer_Tick;
+
+            LoadImagesFromFolder();
+            AssignIconsToSquares();
+        }
+
+        //Load hinh từ ảnh
+        private void LoadImagesFromFolder()
+        {
+            string path = Path.Combine(Application.StartupPath, "img");
+
+            //kiểm tra thư mục tồn tại hay không
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path)
+                    .Where(f => 
+                    f.EndsWith(".jpg") ||
+                    f.EndsWith(".gif") ||
+                    f.EndsWith(".bmp")).ToArray();
+
+                //Chỉ lấy 8 ảnh
+                for (int i = 0; i < 8 && i < files.Length; i++)
+                {
+                    //Đọc file thành mảng byte
+                    byte[] bytes = File.ReadAllBytes(files[i]);
+                    //Tạo Memory stream
+                    MemoryStream ms = new MemoryStream(bytes);
+                    //chuyen thanh Image
+                    Image img = Image.FromStream(ms);
+
+                    //Add 2 lan de tao 2 hinh giong nhau
+                    icons.Add(img); 
+                    icons.Add(img); 
+                }
+            }
+
+        }
+
+        //Gan hinh cho picture box
+        private void AssignIconsToSquares()
+        {
+            //Lap tu pic0 - pic15
+            for (int i = 0; i < 16; i++)
+            {
+                //Het thi dung
+                if (icons.Count == 0)
+                    return;
+
+                // Tìm PictureBox trong tableLayoutPanel
+                PictureBox pic = tableLayoutPanel1.Controls["pic" + i] as PictureBox;
+
+                if (pic == null)
+                {
+                    MessageBox.Show("Không tìm thấy pic" + i);
+                    continue;
+                }
+
+                int index = random.Next(icons.Count);
+                // Lưu hình thật vào Tag
+                pic.Tag = icons[index];
+                // Ban đầu ẩn hình
+                pic.Image = null;
+
+                pic.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                pic.BackColor = Color.LightSeaGreen;
+
+                pic.BorderStyle = BorderStyle.FixedSingle;
+                //Gan su kien Click
+                pic.Click += pictureBox_Click;
+                //Xoa hinh da dung khoi danh sach
+                icons.RemoveAt(index);
+            }
+
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            //neu timer dang chay thi khong cho Click 
+            if (gameTimer.Enabled == true)
+                return;
+            //Ep sender thanh picture box
+            PictureBox clickedPicture = sender as PictureBox;
+
+            if (clickedPicture == null)
+                return;
+
+            // Neu da mo thi bo qua
+            if (clickedPicture.Image != null)
+                return;
+
+            // Hiện hình
+            clickedPicture.Image = (Image)clickedPicture.Tag;
+
+            // Chọn hình đầu tiên
+            if (firstClicked == null)
+            {
+                firstClicked = clickedPicture;
+                return;
+            }
+
+            // Chọn hình thứ hai
+            secondClicked = clickedPicture;
+
+            // Nếu giống nhau
+            if (firstClicked.Tag == secondClicked.Tag)
+            {
+                firstClicked = null;
+                secondClicked = null;
+
+                CheckWin();
+
+                return;
+            }
+            // Nếu khác nhau -> đợi úp
+            gameTimer.Start();
+
+        }
+
+        // KIỂM TRA THẮNG
+        private void CheckWin()
+        {
+            //Duyet qua tat ca picture box
+            foreach (PictureBox pic in tableLayoutPanel1.Controls.OfType<PictureBox>())
+            {
+                if (pic.Image == null)
+                    return;
+            }
+            //In cau thong bao
+            MessageBox.Show("Hoàn thành trò chơi!");
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            gameTimer.Stop();
+            //up lai 2 hinh
+            firstClicked.Image = null;
+            secondClicked.Image = null;
+
+            //Reset lua chon
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
